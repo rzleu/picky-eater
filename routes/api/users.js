28900f21b1636dd5e5e21371e0e486564bf74619
cindyjiang <cindyj301@gmail.com>
2021-09-19 17:11:53 -0500

    complete backend user auth

---
+++ routes/api/users.js
@@ -0,0 +1,115 @@
+/* eslint-disable no-shadow */
+/* eslint-disable consistent-return */
+const express = require('express');
+
+const router = express.Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const passport = require('passport');
+const keys = require('../../config/keys');
+
+const User = require('../../models/User');
+const validateSignupInput = require('../../validation/signup');
+const validateLoginInput = require('../../validation/login');
+
+router.get('/test', (req, res) =>
+  res.json({ msg: 'This is the users route' }),
+);
+
+router.post('/signup', (req, res) => {
+  const { errors, isValid } = validateSignupInput(req.body);
+
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
+  User.findOne({ email: req.body.email }).then((user) => {
+    // add addtional username check?
+    if (user) {
+      errors.email = 'User already exists';
+      return res.status(400).json(errors);
+    }
+    const newUser = new User({
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password,
+    });
+
+    bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(newUser.password, salt, (err, hash) => {
+        if (err) throw err;
+        newUser.password = hash;
+        newUser
+          .save()
+          .then((user) => {
+            const payload = { id: user.id, email: user.email };
+
+            jwt.sign(
+              payload,
+              keys.secretOrKey,
+              { expiresIn: 3600 },
+              (_, token) => {
+                res.json({
+                  success: true,
+                  token: `Bearer ${token}`,
+                });
+              },
+            );
+          })
+          .catch((err) => console.log(err));
+      });
+    });
+  });
+});
+
+router.post('/login', (req, res) => {
+  const { errors, isValid } = validateLoginInput(req.body);
+
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
+  const { username, password } = req.body.username;
+
+  User.findOne({ username }).then((user) => {
+    if (!user) {
+      errors.username = 'This user does not exist';
+      return res.status(400).json(errors);
+    }
+
+    bcrypt.compare(password, user.password).then((isMatch) => {
+      if (isMatch) {
+        const payload = { id: user.id, username: user.username };
+
+        jwt.sign(
+          payload,
+          keys.secretOrKey,
+          { expiresIn: 3600 },
+          (err, token) => {
+            res.json({
+              success: true,
+              token: `Bearer ${token}`,
+            });
+          },
+        );
+      } else {
+        errors.password = 'Incorrect password';
+        return res.status(400).json(errors);
+      }
+    });
+  });
+});
+
+router.get(
+  '/current',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    res.json({
+      id: req.user.id,
+      handle: req.user.handle,
+      email: req.user.email,
+    });
+  },
+);
+
+module.exports = router;
