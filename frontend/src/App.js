657e7e9e2f006f2b56c1197dd7a92c4171fe3dcd
Daniel Mechenko <dmechenko@gmail.com>
2021-09-20 17:16:45 -0400

    Create SplashPage and styling

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import { AuthRoute } from './util/routeUtil';
 import LoginForm from './components/session/Login';
-import './index.css'
+import './util.css';
+import './index.css';
+import SplashPage from './pages/splashPage/SplashPage';
 
 function App() {
   return (
-    <div><LoginForm/></div>
-  )
+    <div className="h-100">
+      <Switch>
+        <Route>
+          <SplashPage />
+        </Route>
+        <AuthRoute path="/">
+          <LoginForm />
+        </AuthRoute>
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
---
+++ frontend/src/pages/splashPage/SplashPage.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './splashpage.module.css';
+
+function SplashPage() {
+  return (
+    <div className="splash-container h-100">
+      <img src="#" alt="" />
+      <h1>Tinder</h1>
+      <h1>Swipe Right</h1>
+      <div className="button-container">
+        <button>Create Account</button>
+        <button>Log In</button>
+      </div>
+    </div>
+  );
+}
+
+export default SplashPage;
