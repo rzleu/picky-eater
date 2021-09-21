d1972119af1cb2dc0208771d4ad8fdf41e80883e
Daniel Mechenko <dmechenko@gmail.com>
2021-09-21 09:46:58 -0400

    Finished '/' page styling

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,17 +5,15 @@ import LoginForm from './components/session/Login';
 import './util.css';
 import './index.css';
 import SplashPage from './pages/splashPage/SplashPage';
+import SignupForm from './components/session/Signup';
 
 function App() {
   return (
     <div className="h-100">
       <Switch>
-        <Route>
-          <SplashPage />
-        </Route>
-        <AuthRoute path="/">
-          <LoginForm />
-        </AuthRoute>
+        <Route exact path="/" component={SplashPage} />
+        <AuthRoute exact path="/login" component={LoginForm} />
+        <AuthRoute exact path="/signup" component={SignupForm} />
       </Switch>
     </div>
   );
--- frontend/src/pages/splashPage/SplashPage.jsx
+++ frontend/src/pages/splashPage/SplashPage.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import LoginForm from '../../components/session/Login';
+import SignupForm from '../../components/session/Signup';
 import styles from './splashpage.module.css';
 
 function SplashPage() {
@@ -7,8 +9,9 @@ function SplashPage() {
       <h1>pickyEatr</h1>
       <h2>Swipe Right</h2>
       <div className={styles.buttonContainer}>
-        <button>Create Account</button>
-        <button>Log In</button>
+        {/* <button>Create Account</button> */}
+        <SignupForm />
+        <button className={styles.splashBtn2}>Log In</button>
       </div>
     </div>
   );
