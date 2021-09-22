bfed588f2440a61c1bed77a2d44d7dbd38809913
Richard Leung <richardzleung@gmail.com>
2021-09-22 11:36:52 -0700

    socket

--- frontend/src/context/socket.js
+++ frontend/src/context/socket.js
@@ -2,11 +2,13 @@ import { createContext } from 'react';
 import socketio from 'socket.io-client';
 
 export function socket() {
-  let sock = socketio.connect(
-    `http://${window.location.hostname}:5000`,
-  );
+  let sock;
   if (process.env.NODE_ENV === 'production') {
     sock = socketio.connect(process.env.PORT);
+  } else {
+    sock = socketio.connect(
+      `http://${window.location.hostname}:5000`,
+    );
   }
   return sock;
 }
