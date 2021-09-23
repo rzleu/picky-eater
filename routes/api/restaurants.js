ec6f8781f40fdbc64f6217a0bab6b683c64bc451
anthill499 <anthill499@gmail.com>
2021-09-22 23:22:30 -0400

    almost done restaurant routes still have bugs though

--- routes/api/restaurants.js
+++ routes/api/restaurants.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-
+const passport = require('passport');
 const Restaurant = require('../../models/Restaurant');
 const User = require('../../models/User');
 const validateRestaurantInput = require('../../validation/restaurant');
@@ -15,27 +15,51 @@ router.get('/', (req, res) => {
     );
 });
 
-router.post('/', (req, res) => {
-  const favoritedRestaurant = {
-    name: req.body.name,
-    address: req.body.address,
-    category: req.body.category,
-    photo: req.body.photo,
-  };
+router.post(
+  '/',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    const favoritedRestaurant = {
+      name: req.body.name,
+      address: req.body.address,
+      category: req.body.category,
+      photo: req.body.photo,
+    };
 
-  // include user's id along with request?
-  User.findOneAndUpdate(
-    {
-      id: req.body.userId,
-    },
-    { $push: { saved: favoritedRestaurant } },
-  )
-    .then(() => res.json({ restaurant: favoritedRestaurant }))
-    .catch((err) => console.log(err));
-});
+    // include user's id along with request?
+    User.findOneAndUpdate(
+      {
+        id: req.body.userId,
+      },
+      { $push: { saved: favoritedRestaurant } },
+      { new: true },
+    )
+      .then(() => res.json({ restaurant: favoritedRestaurant }))
+      .catch((err) =>
+        res.status(404).json({ msg: 'No restaurant found' }),
+      );
+  },
+);
 
-router.delete('/', (req, res) => {
-  Restaurant.findById(req.body.id).then();
-});
+router.delete(
+  '/',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    const currUser = User.findById(req.body.userId);
+    Restaurant.findById(req.body.id)
+      .then((restaurant) => {
+        const newArr = currUser.filter(
+          (stores) => stores !== restaurant,
+        );
+        currUser.saved = newArr;
+      })
+      .then(() => res.json({ newArr: currUser.saved }))
+      .catch((err) =>
+        res
+          .status(404)
+          .json({ msg: 'Could not remove saved restaurant' }),
+      );
+  },
+);
 
 module.exports = router;
