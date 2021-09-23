ecd3fb813a84a88142f22c66f6c52c2d9d412e49
anthill499 <anthill499@gmail.com>
2021-09-22 21:57:30 -0400

    started restaurant routes

--- routes/api/restaurants.js
+++ routes/api/restaurants.js
@@ -3,9 +3,29 @@ const express = require('express');
 const router = express.Router();
 
 const Restaurant = require('../../models/Restaurant');
+const User = require('../../models/User');
 
 router.get('/test', (req, res) =>
   res.json({ msg: 'This is the restaurants route' }),
 );
+
+router.post('/', (req, res) => {
+  const favoritedRestaurant = {
+    name: req.body.name,
+    address: req.body.address,
+    category: req.body.category,
+    photo: req.body.photo,
+  };
+
+  // include user's id along with request?
+  User.findOneAndUpdate(
+    {
+      id: req.body.userId,
+    },
+    { $push: { favorites: favoritedRestaurant } },
+  )
+    .then(() => res.json({ restaurant: favoritedRestaurant }))
+    .catch((err) => console.log(err));
+});
 
 module.exports = router;
