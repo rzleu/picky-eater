635a7eecc8ddac35a5dae8b2aa22255464267fc2
Daniel Mechenko <dmechenko@gmail.com>
2021-09-24 00:20:45 -0400

    merge for styling

--- frontend/src/pages/About/About.jsx
+++ frontend/src/pages/About/About.jsx
@@ -1,16 +1,12 @@
 import React, { useState } from 'react';
 import styles from './about.module.css';
-import logoTemp from '../../assets/images/logoTemp.png';
 import CindyPic from '../../assets/images/CindyPic.png';
 import AntPic from '../../assets/images/AntPic.png';
-import RichPic from '../../assets/images/RichPic.png';
+import RichPic from '../../assets/images/RichPic.jpeg';
 import DanPic from '../../assets/images/DanPic.png';
-
-import {
-  AnimatePresence,
-  AnimateSharedLayout,
-  motion,
-} from 'framer-motion';
+import gitHub from '../../assets/images/github-alt-brands.png';
+import linkedIn from '../../assets/images/linkedin-brands.png';
+import globe from '../../assets/images/globe-solid.png';
 
 function About() {
   return (
@@ -18,28 +14,44 @@ function About() {
       <h1 className={styles.aboutHeader}>The pickyEatr Team</h1>
       <div className={styles.teamContainer}>
         <div className={styles.teamItems}>
-          <div className={styles.teamItem}>
-            <img
-              src={CindyPic}
-              alt="Cindy Jiang"
-              className={styles.proPic}
-            />
-            <div className={styles.nameHeader}>
-              <h2>Cindy Jiang</h2>
-              <li className={styles.gitHubLink}>
-                Favorite food: Enchiladas
-              </li>
-              <ul>
-                <li className={styles.gitHubLink}>
-                  <a href="#">GitHub</a>
+          <div className={styles.teamItemLeft}>
+            <div className={styles.teamItem}>
+              <img
+                src={CindyPic}
+                alt="Cindy Jiang"
+                className={styles.proPic}
+              />
+              <div className={styles.nameHeader}>
+                <h2>Cindy Jiang</h2>
+                <li className={styles.favFood}>
+                  Favorite food: Cacio e Pepe
                 </li>
-                <li className={styles.linkedInLink}>
-                  <a href="#">LinkedIn</a>
-                </li>
-                <li className={styles.personalSite}>
-                  <a href="#">Personal Site</a>
-                </li>
-              </ul>
+                <ul>
+                  <li className={styles.gitHubLink}>
+                    <a
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      href="https://github.com/cindyj301"
+                    >
+                      <img src={gitHub} alt="gitHub" />
+                    </a>
+                  </li>
+                  <li className={styles.linkedInLink}>
+                    <a
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      href="https://www.linkedin.com/in/jiang-cindy/"
+                    >
+                      <img src={linkedIn} alt="linkedIn" />
+                    </a>
+                  </li>
+                  <li className={styles.personalSite}>
+                    <a href="#">
+                      <img src={globe} alt="personalSite" />
+                    </a>
+                  </li>
+                </ul>
+              </div>
             </div>
           </div>
           <div className={styles.teamItemRight}>
@@ -51,49 +63,79 @@ function About() {
               />
               <div className={styles.nameHeader}>
                 <h2>Anthony Huang</h2>
-                <li className={styles.gitHubLink}>
-                  Favorite food: Enchiladas
+                <li className={styles.favFood}>
+                  Favorite food: Korean Fried Chicken
                 </li>
                 <ul>
                   <li className={styles.gitHubLink}>
-                    <a href="#">GitHub</a>
+                    <a
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      href="https://github.com/anthill499"
+                    >
+                      <img src={gitHub} alt="gitHub" />
+                    </a>
                   </li>
                   <li className={styles.linkedInLink}>
-                    <a href="#">LinkedIn</a>
+                    <a
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      href="https://www.linkedin.com/in/anthonyhuang499/"
+                    >
+                      <img src={linkedIn} alt="linkedIn" />
+                    </a>
                   </li>
                   <li className={styles.personalSite}>
-                    <a href="#">Personal Site</a>
+                    <a href="#">
+                      <img src={globe} alt="personalSite" />
+                    </a>
                   </li>
                 </ul>
               </div>
             </div>
           </div>
-          <div className={styles.teamItem}>
-            <img
-              src={RichPic}
-              alt="Richard Leung"
-              className={styles.proPic}
-            />
-            <div className={styles.nameHeader}>
-              <h2>Richard Leung</h2>
-              <li className={styles.gitHubLink}>
-                Favorite food: Enchiladas
-              </li>
-              <ul>
-                <li className={styles.gitHubLink}>
-                  <a href="#">GitHub</a>
+          <div className={styles.teamItemLeft}>
+            <div className={styles.teamItem}>
+              <img
+                src={RichPic}
+                alt="Richard Leung"
+                className={styles.proPic}
+              />
+              <div className={styles.nameHeader}>
+                <h2>Richard Leung</h2>
+                <li className={styles.favFood}>
+                  Favorite food: Shakshukah
                 </li>
-                <li className={styles.linkedInLink}>
-                  <a href="#">LinkedIn</a>
-                </li>
-                <li className={styles.personalSite}>
-                  <a href="#">Personal Site</a>
-                </li>
-              </ul>
+                <ul>
+                  <li className={styles.gitHubLink}>
+                    <a
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      href="https://github.com/rzleu"
+                    >
+                      <img src={gitHub} alt="gitHub" />
+                    </a>
+                  </li>
+                  <li className={styles.linkedInLink}>
+                    <a
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      href="https://www.linkedin.com/in/richardzleung/"
+                    >
+                      <img src={linkedIn} alt="linkedIn" />
+                    </a>
+                  </li>
+                  <li className={styles.personalSite}>
+                    <a href="#">
+                      <img src={globe} alt="personalSite" />
+                    </a>
+                  </li>
+                </ul>
+              </div>
             </div>
           </div>
           <div className={styles.teamItemRight}>
-            <div className={styles.teamItem}>
+            <div className={styles.teamItemDan}>
               <img
                 src={DanPic}
                 alt="Daniel Mechenko"
@@ -101,19 +143,33 @@ function About() {
               />
               <div className={styles.nameHeader}>
                 <h2>Daniel Mechenko</h2>
-                <li className={styles.gitHubLink}>
-                  Favorite food: Enchiladas
+                <li className={styles.favFood}>
+                  Favorite food: Thai Green Curry
                 </li>
-                <div>
+                <div className={styles.needsWidth}>
                   <ul>
                     <li className={styles.gitHubLink}>
-                      <a href="#">GitHub</a>
+                      <a
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        href="https://github.com/dmechenko"
+                      >
+                        <img src={gitHub} alt="gitHub" />
+                      </a>
                     </li>
                     <li className={styles.linkedInLink}>
-                      <a href="#">LinkedIn</a>
+                      <a
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        href="https://www.linkedin.com/in/dmechenko/"
+                      >
+                        <img src={linkedIn} alt="linkedIn" />
+                      </a>
                     </li>
                     <li className={styles.personalSite}>
-                      <a href="#">Personal Site</a>
+                      <a href="#">
+                        <img src={globe} alt="personalSite" />
+                      </a>
                     </li>
                   </ul>
                 </div>
