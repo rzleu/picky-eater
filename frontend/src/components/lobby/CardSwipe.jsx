ac386730852e0af0d67c787bfd1959a085a75b14
dmechenko <dmechenko@gmail.com>
2021-10-10 20:40:31 -0400

    Change styling of modals

--- frontend/src/components/lobby/CardSwipe.jsx
+++ frontend/src/components/lobby/CardSwipe.jsx
@@ -33,7 +33,7 @@ function CardSwipe({ masterList = [] }) {
   const leftSwipe = useRef(null);
   const rightSwipe = useRef(null);
   const rightSwipeList = useRef([]);
-
+  const buttonClicked = useRef();
   let startX = useRef(null);
 
   const handleMasterList = useCallback((list) => {
@@ -106,6 +106,9 @@ function CardSwipe({ masterList = [] }) {
   }, [masterListCopy, socket]);
 
   const handleSave = () => {
+    if (buttonClicked.current) {
+      buttonClicked.current.setAttribute('disabled', 'disabled');
+    }
     dispatch(saveRestaurant(match, userId));
   };
 
@@ -199,128 +202,133 @@ function CardSwipe({ masterList = [] }) {
     <div className={style.swipeContainer}>
       <h2 className={style.swipeHeader}>Swipe Left or Right!</h2>
       <div className={style.cardContainerContainer} id="boundary">
-        <div className={style.cardContainerChild}>
-          <motion.div
-            ref={cardRef}
-            className={`${style.card} card`}
-            style={{
-              x: x,
-              rotate: rotate,
-              cursor: 'grab',
-              border: borderColor,
-              filter: filter,
-            }}
-            drag="x"
-            dragConstraints={{ left: 0, right: 0 }}
-            dragTransition={{
-              bounceStiffness: 600,
-              bounceDamping: 40,
-            }}
-          >
-            {' '}
-            <svg className={style.decisionIcon} viewBox="10 10 30 30">
-              <motion.path
-                fill="none"
-                strokeWidth="2"
-                stroke={strokeColor}
-                d="M14,26 L 22,33 L 35,16"
-                // d="M10.219,1.688c-4.471,0-8.094,3.623-8.094,8.094s3.623,8.094,8.094,8.094s8.094-3.623,8.094-8.094S14.689,1.688,10.219,1.688 M10.219,17.022c-3.994,0-7.242-3.247-7.242-7.241c0-3.994,3.248-7.242,7.242-7.242c3.994,0,7.241,3.248,7.241,7.242C17.46,13.775,14.213,17.022,10.219,17.022 M15.099,7.03c-0.167-0.167-0.438-0.167-0.604,0.002L9.062,12.48l-2.269-2.277c-0.166-0.167-0.437-0.167-0.603,0c-0.166,0.166-0.168,0.437-0.002,0.603l2.573,2.578c0.079,0.08,0.188,0.125,0.3,0.125s0.222-0.045,0.303-0.125l5.736-5.751C15.268,7.466,15.265,7.196,15.099,7.03"
-                strokeDasharray="1"
-                strokeLinejoin="round"
-                // strokeLinecap="round"
-                strokeDashoffset="0"
-                style={{
-                  pathLength: checkStroke,
-                  filter: svgBackdrop,
-                }}
-              />
-              <motion.path
-                fill="none"
-                strokeWidth="2"
-                stroke={strokeColor}
-                d="M17,17 L33,33"
-                // d="M15.898,4.045c-0.271-0.272-0.713-0.272-0.986,0l-4.71,4.711L5.493,4.045c-0.272-0.272-0.714-0.272-0.986,0s-0.272,0.714,0,0.986l4.709,4.711l-4.71,4.711c-0.272,0.271-0.272,0.713,0,0.986c0.136,0.136,0.314,0.203,0.492,0.203c0.179,0,0.357-0.067,0.493-0.203l4.711-4.711l4.71,4.711c0.137,0.136,0.314,0.203,0.494,0.203c0.178,0,0.355-0.067,0.492-0.203c0.273-0.273,0.273-0.715,0-0.986l-4.711-4.711l4.711-4.711C16.172,4.759,16.172,4.317,15.898,4.045z"
-                strokeDasharray="1"
-                // strokeLinecap="round"
-                style={{ pathLength: xStroke1 }}
-              />
-              <motion.path
-                fill="none"
-                strokeWidth="2"
-                stroke={strokeColor}
-                d="M33,17 L17,33"
-                strokeDasharray="1"
-                // strokeLinecap="round"
-                style={{ pathLength: xStroke2 }}
-              />
-            </svg>
-            <img
-              src={masterListCopy[0]?.photos?.[currPhoto]}
-              alt={name}
-              className={style.images}
-            />
-            {currPhoto !== 0 && (
-              <button
-                className={style.photoLeftBtn}
-                onClick={handlePhotoLeftClick}
-              >
-                <ChevronLeft size={64} strokeWidth={3} />
-              </button>
-            )}
-            {currPhoto < 9 && (
-              <button
-                className={style.photoRightBtn}
-                onClick={handlePhotoRightClick}
-              >
-                <ChevronRight strokeWidth={3} size={64} />
-              </button>
-            )}
-            <div
-              className={style.swipeBar}
-              style={{ bottom: infoButtonHidden ? '33%' : '12%' }}
+        {!match && (
+          <div className={style.cardContainerChild}>
+            <motion.div
+              ref={cardRef}
+              className={`${style.card} card`}
+              style={{
+                x: x,
+                rotate: rotate,
+                cursor: 'grab',
+                border: borderColor,
+                filter: filter,
+              }}
+              drag="x"
+              dragConstraints={{ left: 0, right: 0 }}
+              dragTransition={{
+                bounceStiffness: 600,
+                bounceDamping: 40,
+              }}
             >
-              <button ref={leftSwipe} onClick={handleLeftSwipe}>
-                <img
-                  className={style.leftSwipe}
-                  src={leftSwipeBtn}
-                  alt="swipeLeft"
-                />
-              </button>
-              <button
-                className={style.infoCircle}
-                onClick={handleInfoButton}
+              {' '}
+              <svg
+                className={style.decisionIcon}
+                viewBox="10 10 30 30"
               >
-                <Info size={36} strokeWidth={3} />
-              </button>
-              <button ref={rightSwipe} onClick={handleRightSwipe}>
-                <img src={rightSwipeBtn} alt="right" />
-              </button>
-            </div>
-            <div className={style.infoBlock}>
-              {infoButtonHidden ? (
-                <h3>{name}</h3>
-              ) : (
-                <h3 className={style.visibleHeader}>{name}</h3>
+                <motion.path
+                  fill="none"
+                  strokeWidth="2"
+                  stroke={strokeColor}
+                  d="M14,26 L 22,33 L 35,16"
+                  // d="M10.219,1.688c-4.471,0-8.094,3.623-8.094,8.094s3.623,8.094,8.094,8.094s8.094-3.623,8.094-8.094S14.689,1.688,10.219,1.688 M10.219,17.022c-3.994,0-7.242-3.247-7.242-7.241c0-3.994,3.248-7.242,7.242-7.242c3.994,0,7.241,3.248,7.241,7.242C17.46,13.775,14.213,17.022,10.219,17.022 M15.099,7.03c-0.167-0.167-0.438-0.167-0.604,0.002L9.062,12.48l-2.269-2.277c-0.166-0.167-0.437-0.167-0.603,0c-0.166,0.166-0.168,0.437-0.002,0.603l2.573,2.578c0.079,0.08,0.188,0.125,0.3,0.125s0.222-0.045,0.303-0.125l5.736-5.751C15.268,7.466,15.265,7.196,15.099,7.03"
+                  strokeDasharray="1"
+                  strokeLinejoin="round"
+                  // strokeLinecap="round"
+                  strokeDashoffset="0"
+                  style={{
+                    pathLength: checkStroke,
+                    filter: svgBackdrop,
+                  }}
+                />
+                <motion.path
+                  fill="none"
+                  strokeWidth="2"
+                  stroke={strokeColor}
+                  d="M17,17 L33,33"
+                  // d="M15.898,4.045c-0.271-0.272-0.713-0.272-0.986,0l-4.71,4.711L5.493,4.045c-0.272-0.272-0.714-0.272-0.986,0s-0.272,0.714,0,0.986l4.709,4.711l-4.71,4.711c-0.272,0.271-0.272,0.713,0,0.986c0.136,0.136,0.314,0.203,0.492,0.203c0.179,0,0.357-0.067,0.493-0.203l4.711-4.711l4.71,4.711c0.137,0.136,0.314,0.203,0.494,0.203c0.178,0,0.355-0.067,0.492-0.203c0.273-0.273,0.273-0.715,0-0.986l-4.711-4.711l4.711-4.711C16.172,4.759,16.172,4.317,15.898,4.045z"
+                  strokeDasharray="1"
+                  // strokeLinecap="round"
+                  style={{ pathLength: xStroke1 }}
+                />
+                <motion.path
+                  fill="none"
+                  strokeWidth="2"
+                  stroke={strokeColor}
+                  d="M33,17 L17,33"
+                  strokeDasharray="1"
+                  // strokeLinecap="round"
+                  style={{ pathLength: xStroke2 }}
+                />
+              </svg>
+              <img
+                src={masterListCopy[0]?.photos?.[currPhoto]}
+                alt={name}
+                className={style.images}
+              />
+              {currPhoto !== 0 && (
+                <button
+                  className={style.photoLeftBtn}
+                  onClick={handlePhotoLeftClick}
+                >
+                  <ChevronLeft size={64} strokeWidth={3} />
+                </button>
               )}
-              {infoButtonHidden && (
-                <div className={style.testing}>
-                  <p>Rating: {rating}</p>
-                  <p>Phone Number: {phone}</p>
-                  <p>Address: {address}</p>
-                  <p>
-                    <a
-                      href={website}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      Website
-                    </a>
-                  </p>
-                </div>
+              {currPhoto < 9 && (
+                <button
+                  className={style.photoRightBtn}
+                  onClick={handlePhotoRightClick}
+                >
+                  <ChevronRight strokeWidth={3} size={64} />
+                </button>
               )}
-            </div>
-          </motion.div>
-        </div>
+              <div
+                className={style.swipeBar}
+                style={{ bottom: infoButtonHidden ? '33%' : '12%' }}
+              >
+                <button ref={leftSwipe} onClick={handleLeftSwipe}>
+                  <img
+                    className={style.leftSwipe}
+                    src={leftSwipeBtn}
+                    alt="swipeLeft"
+                  />
+                </button>
+                <button
+                  className={style.infoCircle}
+                  onClick={handleInfoButton}
+                >
+                  <Info size={36} strokeWidth={3} />
+                </button>
+                <button ref={rightSwipe} onClick={handleRightSwipe}>
+                  <img src={rightSwipeBtn} alt="right" />
+                </button>
+              </div>
+              <div className={style.infoBlock}>
+                {infoButtonHidden ? (
+                  <h3>{name}</h3>
+                ) : (
+                  <h3 className={style.visibleHeader}>{name}</h3>
+                )}
+                {infoButtonHidden && (
+                  <div className={style.testing}>
+                    <p>Rating: {rating}</p>
+                    <p>Phone Number: {phone}</p>
+                    <p>Address: {address}</p>
+                    <p>
+                      <a
+                        href={website}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        Website
+                      </a>
+                    </p>
+                  </div>
+                )}
+              </div>
+            </motion.div>
+          </div>
+        )}
       </div>
       <CSSTransition
         in={!!match}
@@ -331,25 +339,36 @@ function CardSwipe({ masterList = [] }) {
         <div className={style.matchContainer}>
           <div>
             <div className={style.matchHeading}>
-              <h3>You've got a match!</h3>
-              <span>
-                How does{' '}
-                <a href={website} rel="noreferrer" target="_blank">
-                  {match.name}
-                </a>{' '}
-                sound?
-              </span>
+              {/* <h3>You've got a match!</h3> */}
+              <h3>
+                You've matched on{' '}
+                <span>
+                  <a href={website} rel="noreferrer" target="_blank">
+                    {match.name}
+                  </a>
+                </span>
+                ! {/* sound? */}
+              </h3>
             </div>
 
             <div className={style.bg} />
+            {/* <div className={style.matchButtonContainer}> */}
+            <button
+              className={style.reload}
+              onClick={() => history.go(0)}
+            >
+              Lobby
+            </button>
+
+            <button
+              ref={buttonClicked}
+              className={style.reload}
+              onClick={() => handleSave()}
+            >
+              Save Match
+            </button>
           </div>
-          <button
-            className={style.reload}
-            onClick={() => history.go(0)}
-          >
-            Reload?
-          </button>
-          <button onClick={() => handleSave()}>Star</button>
+          {/* </div> */}
         </div>
       </CSSTransition>
     </div>
